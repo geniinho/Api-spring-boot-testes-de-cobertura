3cd65810f4e2a00f315adc516f4770c79309c7d9
Geniinho <geniinho@hotmail.com>
2021-11-07 09:34:26 -0300

    excecao para email já cadastrado

--- src/main/java/br/com/geninho/api/repositories/UserRepository.java
+++ src/main/java/br/com/geninho/api/repositories/UserRepository.java
@@ -4,6 +4,9 @@ import br.com.geninho.api.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findyByEmail(String email);
 }
